f334fcc3b35c73a050d9c62a81e6cfa09155c3c6
monichre <liamhellis@gmail.com>
2021-03-09 15:29:25 -0500

    new casing paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { Layout } from "../components/Layout/Layout";
-
-import GithubStats from "../components/GithubStats";
-
+import Layout from "../components/Layout/Layout";
+import GithubStats from "../components/GithubStats/GithubStats";
 import ProfileBanner from "../components/ProfileBanner";
 
 export interface HomeProps {}
